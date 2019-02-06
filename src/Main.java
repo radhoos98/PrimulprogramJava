ed834c38ab507f4ef54324efb9b4e9918a3cf51c
radhoos98 <radhoo98@gmail.com>
2019-02-06 20:34:51 +0200

    2nd trial

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       int x=10;
+       int y = 5;
+
+        int adunare = x + y;
+        System.out.println(adunare);
+
     }
 }
