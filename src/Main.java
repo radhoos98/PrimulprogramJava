83e5ecf5a5ecc3f083bfde72ae5bcb8e8b9783ff
radhoos98 <radhoo98@gmail.com>
2019-02-11 15:28:26 +0200

    home trial

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,19 @@ public class Main {
         System.out.println(adunare);
         int scadere = x-y ;
         System.out.println("scadere=" +scadere);
+        System.out.println(x/y);
+        int impartire = x/y;
+        System.out.println(impartire);
+        System.out.println("impartire=" +impartire);
+        int inmultire = x*y ;
+        System.out.println(x*y);
+        System.out.println("inmultire=" +inmultire);
+        //prints "inmultire=50 in the terminal window
+
+
 
     }
 }
+
+
+
