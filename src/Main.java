75b7776fd2ec299f77ff5c6322fcd082a7004797
radhoos98 <radhoo98@gmail.com>
2019-02-13 19:27:03 +0200

    ClassWork

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,47 @@
 public class Main {
 
     public static void main(String[] args) {
-        int x=7;
-        int y=8;
-        System.out.println("hello");
-        System.out.println("Radu Serbanescu");
+//        double x = 7;
+//        int y = 8;
+//        System.out.println("hello");
+//        System.out.println("Radu Serbanescu");
+//
+//        double adunare = x + y;
+//        System.out.println(x + y);
+//        System.out.println("adunare=" + adunare);
+//        double impartire = y / x;
+//        System.out.println("impartire" + impartire);
+//
+//        System.out.println(-5 + 8 * 6);
+//        System.out.println((55 + 9) % 9);
+//        System.out.println(20 + -3 * 5 / 8);
+//        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
 
-        int adunare=x+y;
-        System.out.println(x+y);
-        System.out.println("adunare=" +adunare);
-        double impartire=y/x;
-        System.out.println("impartire" +impartire);
+        int res = scadere(2,3);
+        System.out.println(res);
+        System.out.println(scadere(2,3));
+        System.out.println(inmultire(2,3));
+        System.out.println(impartire (8,4));
+        System.out.println(scadere(21,11));
 
-        System.out.println(-5 +8 *6);
-        System.out.println((55+9)%9);
-        System.out.println(20 + -3*5 /8);
-        System.out.println(5 +15/3 *2 -8 % 3);
+    }
+
+
+
+    public static int scadere(int primulNumar, int alDoileaNumar) {
+        int resultat = primulNumar - alDoileaNumar;
+        return resultat;
+
+
+    }
+    public static int inmultire(int primulNumar, int alDoileaNumar){
+        int resultat = primulNumar * alDoileaNumar;
+        return resultat;
+    }
+    public static int impartire (int primulNumar, int alDoileaNumar){
+        int resultat = primulNumar / alDoileaNumar;
+        return resultat;
     }
 }
-
 
 
